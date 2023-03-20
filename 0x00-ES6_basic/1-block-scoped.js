81ab65c9a759fa4e7a19f95b7e8a0702ab140d5b
Monemax94 <mondayodoh94gmail.com>
2023-03-20 17:33:40 +0100

    Removed unnecessary statements and added es6 statement

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,7 +3,9 @@ export default function taskBlock (trueOrFalse) {
   const task2 = true;
 
   if (trueOrFalse) {
-    return [task, task2];
+    const task = true; // eslint-disable-line no-unused-vars
+    const task2 = false; // eslint-disable-line no-unused-vars
   }
+
   return [task, task2];
 }
