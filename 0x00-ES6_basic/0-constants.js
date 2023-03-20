587b459996553c7008503eaf21dd21597e78441a
Monemax94 <mondayodoh94gmail.com>
2023-03-20 20:43:24 +0100

    Corrections

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -2,10 +2,11 @@ export function taskFirst () {
   const task = 'I prefer const when I can.';
   return task;
 }
-
+/* eslint-disable no-unused-vars */
 export function getLast () {
   return ' is okay';
 }
+/* eslint-disable no-unused-vars */
 
 export function taskNext () {
   let combination = 'But sometimes let';
--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -2,10 +2,14 @@ export default function taskBlock (trueOrFalse) {
   const task = false;
   const task2 = true;
 
+  /* eslint-disable no-unused-vars */
+
   if (trueOrFalse) {
-    const task = true; // eslint-disable-line no-unused-vars
-    const task2 = false; // eslint-disable-line no-unused-vars
+    const task = true;
+    const task2 = false;
   }
+
+  /* eslint-disable no-unused-vars */
 
   return [task, task2];
 }
