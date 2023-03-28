cbc264b372024427ba532460c2dbd661fe3b8a40
Monemax94 <mondayodoh94gmail.com>
2023-03-28 09:57:39 +0100

    A function that returns an array of objects. Each object have 3 attributes

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,13 @@
+/**
+ * This function retrieves a list of students.
+ * @author Monday Odoh <https://github.com/Monemax94>
+ * @returns {{id: Number, firstName: String, Location: String}[]}
+ */
+export default getListStudents => {
+  return [
+    { id: 1, firstName: 'Monday Odoh', location: 'Asaba' },
+    { id: 2, firstName: 'JohnKennedy', location: 'PHC' },
+    { id: 3, firstName: 'Andrew', location: 'Abuja' },
+    { id: 5, firstName: 'Kenneth', location: 'Lagos' },
+  ];
+}
