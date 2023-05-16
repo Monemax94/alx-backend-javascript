00b2d824277eed74bbcd7da4a46ec0bbf3b262fe
Monemax94 <mondayodoh94gmail.com>
2023-05-16 05:36:08 +0100

    First commit

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+/**
+ * Displays a message on the STDOUT.
+ * @param {String} 'message' The message to display.
+ * @author Monday Odoh <https://github.com/Monemax94>
+ */
+const displayMessage = (message) => {
+  console.log(message);
+};
+
+module.exports = displayMessage;
